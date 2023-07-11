f6851c749b0e4a1d3231f9fd5fdac03fab90bcdf
Thibyaa <thibyaa@gmail.com>
2023-07-11 13:10:44 +0100

    adding in the footer and main body

--- front-page.php
+++ front-page.php
@@ -1,4 +1,51 @@
-<!-- landing page -->
 <?php 
     get_header();
+?>
+<img class="hero_image" src="#" alt="">
+
+<p> ANNOUNCEMENTS: Rolling Text to go here </p>
+
+<section class="update_section">
+    <div>
+        <!-- this will be linked to the academy -->
+        <img src="#" alt="">
+        <h3> UPCOMING CLASS </h3>
+        <!-- rolling text -->
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim </p>
+    </div>
+
+    <div>
+        <img src="#" alt="">
+        <h3> UPCOMING CLASS </h3>
+        <!-- rolling text -->
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim </p>
+    </div>
+</section>
+
+<section class="testimonial_cards">
+    <h2> TESTIMONIALS </h2>
+    <div>
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor </p>
+        <!-- stars to go here -->
+        <p> CLIENT NAME </p>
+    </div>
+    <div>
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor </p>
+        <!-- stars to go here -->
+        <p> CLIENT NAME </p>
+    </div>
+    <div>
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor </p>
+        <!-- stars to go here -->
+        <p> CLIENT NAME </p>
+    </div>
+    <div>
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor </p>
+        <!-- stars to go here -->
+        <p> CLIENT NAME </p>
+    </div>
+</section>
+
+<?php 
+    get_footer();
 ?>
---
+++ page.php
@@ -0,0 +1,16 @@
+<?php
+get_header();
+
+while(have_posts()) {
+  the_post();
+  
+  ?>
+
+  <div>
+     <?php the_content(); ?>
+  </div>
+  
+
+<?php }
+get_footer();
+?>
