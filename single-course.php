d34f504e7e89e138ed3b315ae3972843ac0ae25b
Kelly <kellykl_wong@hotmail.com>
2023-07-12 13:33:02 +0100

    adds content to single-course.php

--- single-course.php
+++ single-course.php
@@ -6,12 +6,60 @@ get_the_title();
 ?>
 
 <div>
-    <?php 
+  <?php 
     $today = date('Ymd');
     $course = new WP_Query(array(
-        'post_type'=>'course'
+        'post_type'=>'course',
+        'posts_per_page' => 1
+        // 'meta_query' => array(
+        //   array(
+        //     'key' => 'course_class',
+        //     'compare' => 'LIKE',
+        //     'value' => '"' . get_the_ID() . '"'
+        //   )
+        // )
     ));
-    ?>
+    
+    if($course->have_post()) {
+      echo '<h2>'. get_the_title() .'</h2>';
+      while($course->the_post()) {
+        $course->the_post(); ?>
+
+        <h4>OVERVIEW OF THE COURSE</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, eveniet! Molestiae esse, tempore consequatur voluptates asperiores atque voluptate, officia ipsum in vitae vel. Reiciendis impedit itaque eius rem quaerat nam.</p>
+        <p>This side to have a snapshot of the following:</p>
+        <ul>
+          <li>Duration of the course </li>
+          <li>Kit provided + details</li>
+          <li>Qualifications that you will receive</li>
+          <li>Course fees?</li>
+        </ul>
+
+        <h4>IS THIS COURSE FOR YOU?</h4>
+        <ul>
+          <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</li>
+          <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</li>
+          <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</li>
+          <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</li>
+          <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</li>
+        </ul>
+
+        <h4>COURSE STRUCTURE</h4>
+        <ul>
+          <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</li>
+          <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</li>
+          <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</li>
+          <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</li>
+          <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</li>
+        </ul>
+
+        <button>CONTACT FOR MORE DETAILS</button>
+      <?php }
+    }
+
+  ?>
+
+
 </div>
 
 <?php
