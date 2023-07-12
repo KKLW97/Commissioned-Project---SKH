432585b15a0a09f766e5413c32afff54cde5d7c9
Kelly <kellykl_wong@hotmail.com>
2023-07-12 16:06:36 +0100

    adds css link to function.php

--- functions.php
+++ functions.php
@@ -3,6 +3,8 @@
 function skh_theme_files () {
     wp_enqueue_style('header_css', get_theme_file_uri('css/header.css'));
     wp_enqueue_style('index_css', get_theme_file_uri('css/index.css'));
+    wp_enqueue_style('footer_css', get_theme_file_uri('css/footer.css'));
+
 }
 
 add_action('wp_enqueue_scripts', 'skh_theme_files');
