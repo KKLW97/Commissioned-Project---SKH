39ab83089bf498335fb9f09aa14a126c7640ee89
Thibyaa <thibyaa@gmail.com>
2023-07-13 10:20:53 +0100

    function added

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
 function skh_theme_files () {
     wp_enqueue_style('header_css', get_theme_file_uri('css/header.css'));
     wp_enqueue_style('index_css', get_theme_file_uri('css/index.css'));
+    wp_enqueue_style('testimonial_css', get_theme_file_uri('css/testimonial.css'));
 }
 
 add_action('wp_enqueue_scripts', 'skh_theme_files');
