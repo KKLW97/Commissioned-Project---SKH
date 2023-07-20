f10b76044fffc554281f64062b1d85ec9013a078
Thibyaa <thibyaa@gmail.com>
2023-07-20 18:31:58 +0100

    nav bar changed to caps

--- header.php
+++ header.php
@@ -9,9 +9,9 @@
   <body <?php body_class(); ?>>
       <div class="announcement_bar">
         <ul> 
-          <li> London </li>
-          <li> Dubai </li>
-          <li> Turkey (Coming Soon) </li>
+          <li> LONDON </li>
+          <li> DUBAI </li>
+          <li> TURKEY (Coming Soon) </li>
         </ul>
       </div>
       <header>
@@ -22,43 +22,43 @@
             <nav>
               <ul>
             <div class="dropdown_menu">
-              <li <?php if(is_page('academy') OR get_post_type()== 'course') echo 'class="current_menu_item"'?>><a href="<?php echo site_url('academy/');?>"> Academy </a>
+              <li <?php if(is_page('academy') OR get_post_type()== 'course') echo 'class="current_menu_item"'?>><a href="<?php echo site_url('academy/');?>"> ACADEMY </a>
                 <i class="fa fa-caret-down" style="font-size: 17px"></i>
                   <div class="dropdown-content">
-                    <a href="<?php echo site_url('/courses/lash-academy/');?>"> Lash Academy </a>
-                    <a href="<?php echo site_url('/courses/brow-academy/');?>"> Brow Academy </a>
+                    <a href="<?php echo site_url('/courses/lash-academy/');?>"> LASH TRAINING </a>
+                    <a href="<?php echo site_url('/courses/brow-academy/');?>"> BROW TRAINING </a>
                   </div>
               </li>
             </div>
 
             <div class="dropdown_menu">
-              <li <?php if(get_post_type()== 'service-categories') echo 'class="current_menu_item"'?>><a> Services </a>
+              <li <?php if(get_post_type()== 'service-categories') echo 'class="current_menu_item"'?>><a> SERVICES </a>
                 <i class="fa fa-caret-down" style="font-size: 17px"></i>
                   <div class="dropdown-content">
-                    <a href="<?php echo site_url('/service_categories/beauty-clinic/');?>"> Beauty Clinic </a>
-                    <a href="<?php echo site_url('/service_categories/lash-bar/');?>"> Lash Bar </a>
+                    <a href="<?php echo site_url('/service_categories/beauty-clinic/');?>"> BEAUTY CLINIC </a>
+                    <a href="<?php echo site_url('/service_categories/lash-bar/');?>"> LASH BAR </a>
                   </div>
               </li>
             </div>
             
               <li <?php if(is_page('shop')) echo 'class="current_menu_item"' ?>>
-                <a href="<?php echo site_url('/service_categories/weight-loss-clinic/');?>"> Weight Loss </a>
+                <a href="<?php echo site_url('/service_categories/weight-loss-clinic/');?>"> WEIGHT LOSS </a>
               </li>
 
               <li <?php if(is_page('shop')) echo 'class="current_menu_item"' ?>>
-                <a href="<?php echo site_url('#');?>"> Shop </a>
+                <a href="<?php echo site_url('#');?>"> SHOP </a>
               </li>
 
               <li <?php if(is_page('team')) echo 'class="current_menu_item"' ?>>
-                <a href="<?php echo site_url('team/');?>"> Team </a>
+                <a href="<?php echo site_url('team/');?>"> TEAM </a>
               </li>
 
               <li <?php if(is_page('contact-us')) echo 'class="current_menu_item"' ?>> 
-                <a href="<?php echo site_url('contact-us/');?>"> Contact </a>
+                <a href="<?php echo site_url('contact-us/');?>"> CONTACT </a>
               </li>
 
               <li>
-                <a href="https://shorturl.at/BEU06"> Book </a>
+                <a href="https://shorturl.at/BEU06"> BOOK </a>
               </li>
 
               <li>
