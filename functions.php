046d85d77f54912dd0ceba27eeb9c11e09d92682
Thibyaa <thibyaa@gmail.com>
2023-07-20 23:02:39 +0100

    nav bar shadow added and second announcement bar font size changed

--- functions.php
+++ functions.php
@@ -60,4 +60,11 @@ function skh_adjust_queries($query){
 }
 add_action('pre_get_posts', 'skh_adjust_queries');
 
+// removes the white space where the admin bar was supposed to be
+add_action('get_header', 'remove_admin_login_header');
+
+function remove_admin_login_header() {
+    remove_action('wp_head', '_admin_bar_bump_cb');
+}
+
 ?>
