a66476ebec5adf611b7f69c434d9d9c3d5f51532
Kelly Wong <kellykl_wong@hotmail.com>
2023-07-20 09:52:41 +0100

    adds opacity on hover and lash.png

--- front-page.php
+++ front-page.php
@@ -49,7 +49,7 @@
         ?>
         <div class="update_component">
             <div class="update_image">
-                <img src=<?php echo get_theme_file_uri('assets/product.png'); ?> alt="">
+                <img src=<?php echo get_theme_file_uri('assets/lash.png'); ?> alt="">
             </div>
             <div class="update_content">
                 <?php echo '<h3> UPCOMING: ' . get_the_title() .' </h3>' ?>
