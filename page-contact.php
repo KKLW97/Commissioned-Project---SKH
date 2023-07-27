81130fdf225f342fbcc86507e43f724b6d126d6b
Thibyaa <thibyaa@gmail.com>
2023-07-27 12:16:09 +0100

    test

--- page-contact.php
+++ page-contact.php
@@ -6,6 +6,8 @@ get_header();
 
 <?php echo do_shortcode('[wpforms id="123"]'); ?>
 
+hi kelly 
+
 <section class="london_map">
 
     <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2482.8779303624297!2d-0.14909723862605828!3d51.51545547159262!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48760d85ce507fa3%3A0x527f152202f97b90!2sS.K.H%20Beauty%20Clinic!5e0!3m2!1sen!2suk!4v1690452927559!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
