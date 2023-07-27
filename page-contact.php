012ee411b29e4e980f90ed431e1b5a9d6c149f11
Thibyaa <thibyaa@gmail.com>
2023-07-27 11:49:18 +0100

    contact form added

--- page-contact.php
+++ page-contact.php
@@ -1,9 +1,13 @@
 <?php 
 get_header();
-
 ?>
 
-<?php
+<h1><?php echo get_the_title(); ?></h1>
 
+<section class="contact_form">
+<?php echo do_shortcode('[wpforms id="123"]'); ?>
+</section>
+
+<?php
 get_footer();
 ?>
