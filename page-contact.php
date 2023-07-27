c5766ff4b010cb8f7838af10496121189dfbe6e0
Kelly Wong <kellykl_wong@hotmail.com>
2023-07-27 16:16:27 +0100

    fixed map overlay issues

--- page-contact.php
+++ page-contact.php
@@ -13,7 +13,7 @@ get_header();
         </div>
 
         <div class="map"> 
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2482.918924246115!2d-0.15266982238182347!3d51.5147034101685!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48760d85ce507fa3%3A0x527f152202f97b90!2sS.K.H%20Beauty%20Clinic!5e0!3m2!1sen!2suk!4v1690469843275!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2482.918924246115!2d-0.15266982238182347!3d51.5147034101685!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48760d85ce507fa3%3A0x527f152202f97b90!2sS.K.H%20Beauty%20Clinic!5e0!3m2!1sen!2suk!4v1690469843275!5m2!1sen!2suk" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
 
         <div class="day">
