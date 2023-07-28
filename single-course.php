20a106e9fb03317f1dda7950fbfa6cf4a151a386
Thibyaa <thibyaa@gmail.com>
2023-07-28 18:04:46 +0100

    button link correcte

--- single-course.php
+++ single-course.php
@@ -110,7 +110,7 @@ $caption = $image['caption']; ?>
 ?>
 </section>
 <div class="skh_contact">
-  <a href="<?php echo site_url('contact-us/');?>"><button class="skh_contact_button">START YOUR JOURNEY WITH SKH</button></a>
+  <a href="<?php echo site_url('contact/');?>"><button class="skh_contact_button">START YOUR JOURNEY WITH SKH</button></a>
 </div>
 
 <section class="instagram">
