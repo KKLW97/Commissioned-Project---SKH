8e860789e29f39b0baee8969037544dd4e6c668d
Thibyaa <thibyaa@gmail.com>
2023-07-31 13:36:02 +0100

    removes image size function

--- functions.php
+++ functions.php
@@ -35,7 +35,6 @@ add_action('wp_enqueue_scripts', 'add_google_fonts');
 function skh_features() {
     add_theme_support('post-thumbnails');
     add_image_size('trainerPortrait', 380, 350, true);
-    add_image_size('serviceImage', 250, 150, true);
 }
 
 add_action('after_setup_theme', 'skh_features');
