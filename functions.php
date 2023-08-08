20566c2bed71fc14967aa25d9690a796fe6480dd
Thibyaa <thibyaa@gmail.com>
2023-08-08 15:16:53 +0100

    removing default woocommerce and adding in woocommerce support. logo fixed.

--- functions.php
+++ functions.php
@@ -96,4 +96,13 @@ function special_nav_class($classes, $item){
     return $classes;
 }
 
+add_action( 'after_setup_theme', 'woocommerce_support' );
+function woocommerce_support() {
+   add_theme_support( 'woocommerce' );
+}   
+
+if (class_exists('Woocommerce')){
+    add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
+}
+
 ?>
